05fac7bc00f8d9c88f2add28709abaf79b88fa6b
Jason Lee <huacnlee@gmail.com>
2017-02-28 10:35:37 +0800

    只有在 Homeland 的 `modules` 配置启用了 `press` 时才启用插件。

--- README.md
+++ README.md
@@ -1,40 +1,52 @@
 Homeland::Press
 --------------
 
-A Press plugin for [Homeland](https://gethomeland.com) .
+头条插件 for [Homeland](https://gethomeland.com) .
 
 ## Usage
 
 ## Installation
 
-Add this line to your Homeland application's Gemfile:
+在 Homeland 应用的 Gemfile 增加:
 
 ```ruby
 gem 'homeland-press'
 ```
 
-And then execute:
+然后执行 `bundle install`
 
-```bash
-$ bundle
+## Configuration
+
+修改 Homeland 的 `modules` 配置，增加 `press` 以启用。
+
+```
+defaults: &defaults
+  # add "press" to modules
+  modules: 'topic,...,press'
 ```
 
 
 ## Contributing
 
-You need use this plugin on Homeland development.
-
-Gemfile
+如果你需要开发此插件，你需要同时准备好 Homeland 项目的开发环境，并修改 Homeland 项目 Gemfile:
 
 ```
 gem 'homeland-press', path: '../homeland-press'
 ````
 
-And run `bundle install` and `rails db:migrate`
+将 homeland-press 路径指向一个上层目录
 
-Now, the plugin has auto mount on Homeland.
+```
+~/work/homeland
+~/work/homeland-press
+```
 
-Visit http://localhost:3000/posts
+然后执行 `bundle install` 和 `rails db:migrate`
+
+现在，插件已经挂上了（别忘了开启 modules 的配置），然后在 Homeland 下面启动 `rails s`
+
+访问 http://localhost:3000/posts 来验证功能。
 
 ## License
+
 The gem is available as open source under the terms of the [MIT License](http://opensource.org/licenses/MIT).
--- lib/homeland/press/engine.rb
+++ lib/homeland/press/engine.rb
@@ -4,23 +4,25 @@ module Homeland
       isolate_namespace Homeland::Press
 
       initializer 'homeland_press.init' do |app|
-        Homeland.register_plugin do |plugin|
-          plugin.name           = 'press'
-          plugin.display_name   = '头条'
-          plugin.description    = 'A Press plugin for Homeland.'
-          plugin.navbar_link    = true
-          plugin.user_menu_link = true
-          plugin.root_path      = "/posts"
-          plugin.spec_path      = config.root.join('spec')
+        if Setting.has_module?(:press)
+          Homeland.register_plugin do |plugin|
+            plugin.name           = 'press'
+            plugin.display_name   = '头条'
+            plugin.description    = 'A Press plugin for Homeland.'
+            plugin.navbar_link    = true
+            plugin.user_menu_link = true
+            plugin.root_path      = "/posts"
+            plugin.spec_path      = config.root.join('spec')
+          end
+
+          User.send :include, Homeland::Press::UserMixin
+
+          app.routes.prepend do
+            mount Homeland::Press::Engine, at: '/posts'
+          end
+
+          app.config.paths["db/migrate"].concat(config.paths["db/migrate"].expanded)
         end
-
-        User.send :include, Homeland::Press::UserMixin
-
-        app.routes.prepend do
-          mount Homeland::Press::Engine, at: '/posts'
-        end
-
-        app.config.paths["db/migrate"].concat(config.paths["db/migrate"].expanded)
       end
     end
   end
