16e6b6ab8862313b9a811be3a6f2f5df3d054171
Jason Lee <huacnlee@gmail.com>
2017-02-28 18:25:42 +0800

    admin 账号发布的时候，作者名称显示为“官方”

--- app/helpers/homeland/press/application_helper.rb
+++ app/helpers/homeland/press/application_helper.rb
@@ -1,5 +1,10 @@
 module Homeland::Press
   module ApplicationHelper
     delegate :comments_path, to: :main_app
+
+    def press_user_name_tag(user)
+      return '官方' if user.admin?
+      user_name_tag(user)
+    end
   end
 end
