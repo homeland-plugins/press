3a6a53890f0ca77b78bbfc1e59fc058344cfe317
Jason Lee <huacnlee@gmail.com>
2017-04-07 10:20:05 +0800

    Version 0.3.4

--- lib/homeland/press/engine.rb
+++ lib/homeland/press/engine.rb
@@ -4,29 +4,30 @@ module Homeland
       isolate_namespace Homeland::Press
 
       initializer 'homeland.press.init' do |app|
-        break unless Setting.has_module?(:press)
-        app.config.after_initialize do
-          # Because need use I18n.t
-          # must after Rails application initialized will get correct locale
-          Homeland.register_plugin do |plugin|
-            plugin.name              = 'press'
-            plugin.display_name      = I18n.t('plugin.press')
-            plugin.description       = 'A Press/News plugin for Homeland.'
-            plugin.navbar_link       = true
-            plugin.admin_navbar_link = true
-            plugin.root_path         = "/posts"
-            plugin.admin_path        = "/admin/posts"
+        if Setting.has_module?(:press)
+          app.config.after_initialize do
+            # Because need use I18n.t
+            # must after Rails application initialized will get correct locale
+            Homeland.register_plugin do |plugin|
+              plugin.name              = 'press'
+              plugin.display_name      = I18n.t('plugin.press')
+              plugin.description       = 'A Press/News plugin for Homeland.'
+              plugin.navbar_link       = true
+              plugin.admin_navbar_link = true
+              plugin.root_path         = "/posts"
+              plugin.admin_path        = "/admin/posts"
+            end
           end
+
+
+          User.send :include, Homeland::Press::UserMixin
+
+          app.routes.prepend do
+            mount Homeland::Press::Engine => '/'
+          end
+
+          app.config.paths["db/migrate"].concat(config.paths["db/migrate"].expanded)
         end
-
-
-        User.send :include, Homeland::Press::UserMixin
-
-        app.routes.prepend do
-          mount Homeland::Press::Engine => '/'
-        end
-
-        app.config.paths["db/migrate"].concat(config.paths["db/migrate"].expanded)
       end
     end
   end
--- lib/homeland/press/version.rb
+++ lib/homeland/press/version.rb
@@ -1,6 +1,6 @@
 module Homeland
   module Press
     NAME    = 'press'
-    VERSION = '0.3.3'
+    VERSION = '0.3.4'
   end
 end
