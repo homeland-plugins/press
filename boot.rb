2118aee57332ff97fd3fd19b639f013439942fd1
Jason Lee <huacnlee@gmail.com>
2020-02-15 17:14:55 +0800

    Fix version info

--- boot.rb
+++ boot.rb
@@ -8,6 +8,7 @@ Homeland.register_plugin do |plugin|
   plugin.name              = 'press'
   plugin.display_name      = '头条'
   plugin.description       = '头条栏目插件，可以独立发布官方内容。'
+  plugin.version           = Homeland::Press::VERSION
   plugin.navbar_link       = true
   plugin.admin_navbar_link = true
   plugin.root_path         = '/posts'
